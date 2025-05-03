3ea124245b6d6fec22651d65a9d60ca33a67cd0b
Ivann Alcantara <ivann850@gmail.com>
2025-05-02 23:52:13 -0400

    adding search

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,6 +4,8 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearcParams} from 'next/navigation';
+
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
